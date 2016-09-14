f3db4e4d78841d8168ff17b62e751355e297d685
_michal <sz-m@users.noreply.github.com>
2016-09-14 16:58:07 +0200

    ipaddress now show only ipv4

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/whoami', function(req, res){
-  var language = req.headers['accept-language'].split(',')[0];
+  var ip = req.ip.substring(req.ip.lastIndexOf(':')+1, req.ip.length);
 
+  var language = req.headers['accept-language'].split(',')[0];
+  
   var userAgent = req.headers['user-agent'];
   var software = userAgent.substring(userAgent.indexOf('(')+1, userAgent.indexOf(')')).replace(';', ',');
 
   var result = {
-    'ipaddress': req.ip,
+    'ipaddress': ip,
     'language': language,
     'software': software
   };
