567efa0cab7381bc87972e28439a6cb056c05adc
_michal <sz-m@users.noreply.github.com>
2016-09-14 16:10:07 +0200

    remove useless code

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ app.use(express.static('public'));
 app.get('/:time', function(req, res){
   if(req.params.time)
     res.json(jt.jsonTime(req.params.time));
-  else
-    res.sendfile('index.html');
 });
 
 app.listen(process.env.PORT || 8080);
